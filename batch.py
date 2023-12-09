6fcf59229ad0cb877384123e961a897fc8e774c6
gshine <gshine@m.scnu.edu.cn>
2023-12-09 14:38:55 +0800

    patch batch.py

--- batch.py
+++ batch.py
@@ -22,11 +22,6 @@ def move_html_files(source_dir, target_dir):
     dirs = glob.glob(os.path.join(source_dir, "*/"))
 
     for dir_path in dirs:
-        # 将源目录名替换为目标目录名
-        target_path = dir_path.replace(source_dir, target_dir)
-        # 如果目标目录不存在，则创建目录
-        if not os.path.exists(target_path):
-            os.makedirs(target_path)
 
         # 获取目录下的所有HTML文件
         html_files = glob.glob(os.path.join(dir_path, "*.html"))
@@ -39,10 +34,10 @@ def move_html_files(source_dir, target_dir):
             file_name = os.path.basename(file_path)
 
             # 移动文件到目标目录
-            shutil.move(file_path, os.path.join(target_path, file_name))
+            shutil.move(file_path, os.path.join(target_dir, file_name))
 
 # 调用函数进行移动
 source_directory = "notes/"
 target_directory = "docs/"
-repo_url = "https://github.com/LoongGshine/LKI"
+repo_url = "https://github.com/LoongGshine/LKI/blob/main"
 move_html_files(source_directory, target_directory)
